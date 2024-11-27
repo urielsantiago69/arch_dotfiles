1a0452bbee2dc5f04d93ee74c30b653cd8d897fe
Uriel Santiago <121160292+urielsantiago69@users.noreply.github.com>
2024-11-27 09:51:00 +0900

    November 27, 2024 09:50 AM
    Updated nvchad. New init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
@@ -15,7 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 vim.o.wrap = true
 vim.o.linebreak = true
 
-
 local lazy_config = require "configs.lazy"
 
 -- load plugins
@@ -25,9 +24,6 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
   },
 
   { import = "plugins" },
@@ -37,6 +33,7 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+require "options"
 require "nvchad.autocmds"
 
 vim.schedule(function()
